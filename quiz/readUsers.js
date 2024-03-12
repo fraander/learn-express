371647318c1b4f48ba1f9997fdf733441fc85cf5
fraander <57777918+fraander@users.noreply.github.com>
2024-03-12 16:02:01 -0400

    refactor

--- quiz/readUsers.js
+++ quiz/readUsers.js
@@ -1,2 +1,28 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
+
+router.get("/read/username/:u", (req, res) => {
+  const u = req.params.u;
+
+  console.log(u);
+  const filtered = users
+    .filter(function (user) {
+      return user.username === u;
+    })
+    .map(function (user) {
+      return { id: user.id, email: user.email };
+    });
+
+  console.log(filtered);
+
+  res.send(filtered);
+});
+
+router.use("/read/usernames", addMsgToRequest);
+
+router.get("/read/usernames", (req, res) => {
+  let usernames = req.users.map(function (user) {
+    return { id: user.id, username: user.username };
+  });
+  res.send(usernames);
+});
--- quiz/writeUsers.js
+++ quiz/writeUsers.js
@@ -1,4 +1,31 @@
-const express = require('express')
-const fs = require('fs');
-const path = require('path');
-const router = express.Router()
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const router = express.Router();
+
+const addMsgToRequest = function (req, res, next) {
+  if (users) {
+    req.users = users;
+    next();
+  } else {
+    return res.json({
+      error: { message: "users not found", status: 404 },
+    });
+  }
+};
+
+router.use("/write/adduser", addMsgToRequest);
+
+router.post("/write/adduser", (req, res) => {
+  let newuser = req.body;
+  req.users.push(newuser);
+  fs.writeFile(
+    path.resolve(__dirname, "../data/users.json"),
+    JSON.stringify(req.users),
+    (err) => {
+      if (err) console.log("Failed to write");
+      else console.log("User Saved");
+    }
+  );
+  res.send("done");
+});
